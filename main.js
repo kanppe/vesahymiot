7301302e3ff038979dfb4c0f1ad7f8066b1f25e8
knappe <knappe@gmail.com>
2016-11-13 16:00:06 +0200

    files

---
+++ main.js
@@ -0,0 +1,284 @@
+var smileys = {
+    "mmmhmm" : "http://www.netikka.net/jorge/hymiot/misc/rhmm.gif",
+    "smug" : "http://i.somethingawful.com/forumsystem/emoticons/emot-smug.gif",
+    "rink" : "http://www.tirpax.com/kema/kuvat/rink.gif",
+    "aaa" : "http://www.netikka.net/jorge/hymiot/gifs/elftor.gif",
+    "hmm" : "http://www.tirpax.com/kema/kuvat/scratchchin.gif",
+    "hmmm" : "http://www.tirpax.com/kema/kuvat/scratchchin.gif",
+    "nyrillataa" : "http://static.iltalehti.fi/terveys/kesakilot_jutilaSTL_tr.jpg",
+    "dx" : "http://tirpax.com/kema/kuvat/invertxd.gif",
+    "peukku" : "http://futisforum2.fi/Smileys/original/thumbup.gif",
+    "kyl" : "http://www.netikka.net/jorge/hymiot/misc/o.gif",
+    "snus" : "http://www.netikka.net/jorge/hymiot/misc/nuuska2ct3al.gif",
+    "asd" : "http://www.netikka.net/jorge/hymiot/misc/flllaen.gif",
+    "kuola" : "http://www.netikka.net/jorge/hymiot/misc/drool.gif",
+    "drool" : "http://www.netikka.net/jorge/hymiot/misc/drool.gif",
+    "puntti" : "http://www.netikka.net/jorge/hymiot/misc/punttihn7.gif",
+    "jogi" : "http://www.netikka.net/jorge/hymiot/smoke/avatar1.gif",
+    "cthink" : "http://www.netikka.net/jorge/hymiot/think/coolthink.gif",
+    "wtf" : "http://www.netikka.net/jorge/hymiot/think/lolwtf.gif",
+    "coolthink" : "http://www.netikka.net/jorge/hymiot/think/coolthink0yc.gif",
+    "isopaathink" : "http://www.netikka.net/jorge/hymiot/think/vaikeapaatos3.gif",
+    "toruneut" : "http://www.netikka.net/jorge/hymiot/toru/torunassu7yb.gif",
+    "torucool" : "http://www.netikka.net/jorge/hymiot/toru/eltoru0jl.gif",
+    "huoh" : "http://www.netikka.net/jorge/hymiot/neutral/huoh9rg.gif",
+    "isopaa" : "http://www.netikka.net/jorge/hymiot/neutral/isopaa.gif",
+    "nute" : "http://www.netikka.net/jorge/hymiot/neutral/neutralll.gif",
+    "nasd" : "http://www.netikka.net/jorge/hymiot/neutral/iconneutralga6.gif",
+    "casd" : "http://www.netikka.net/jorge/hymiot/cool/coooool.gif",
+    "crg" : "http://www.netikka.net/jorge/hymiot/cool/coolrg9.gif",
+    "herne" : "http://www.netikka.net/jorge/hymiot/misc/nenuherne.gif",
+    "ihast" : "http://img.photobucket.com/albums/v484/tsurai87/vesa/emot-allears.gif",
+    "doh" : "http://i.somethingawful.com/forumsystem/emoticons/emot-doh.gif",
+    "failrimshot" : "http://i.somethingawful.com/forumsystem/emoticons/emot-downsrim.gif",
+    "ohno" : "http://fi.somethingawful.com/images/smilies/emot-nyd.gif",
+    "ssh" : "http://i.somethingawful.com/forumsystem/emoticons/emot-ssh.gif",
+    "jahas" : "http://i.somethingawful.com/forumsystem/emoticons/emot-colbert.gif",
+    "clol" : "http://www.netikka.net/jorge/hymiot/cool/coollol.gif",
+    "nam" : "http://www.netikka.net/jorge/hymiot/misc/iconsmile2ip8.gif",
+    "ploeoe" : "http://www.jkry.org/~drsnu/misc/ploeoe.gif",
+    "myh" : "http://www.jkry.org/~drsnu/misc/myh.gif",
+    "tsufe" : "http://www.netikka.net/jorge/hymiot/misc/tsufe.gif",
+    "lach" : "http://www.netikka.net/jorge/hymiot/misc/lach.gif",
+    "hhmm" : "http://i354.photobucket.com/albums/r425/progeda/hmm.png",
+    "red" : "http://www.netikka.net/jorge/hymiot/misc/redface.gif",
+    "aia" : "http://www.jkry.org/~drsnu/misc/aea.gif",
+    "natsi" : "http://dongato.dyndns.org/icon/yellow/icon_salut.gif",
+    "emt" : "http://forums.nasioc.com/forums/images/smilies/blankface.gif",
+    "muna" : "http://www.jkry.org/~drsnu/misc/munaoi1.gif",
+    "viilto" : "http://www.jkry.org/~drsnu/misc/emoslice0mp.gif",
+    "bileet" : "http://www.jkry.org/~drsnu/misc/party.gif",
+    "mic" : "http://www.jkry.org/~drsnu/misc/106.gif",
+    "sauna" : "http://www.jkry.org/~drsnu/misc/sauna.gif",
+    "nag" : "http://www.jkry.org/~drsnu/misc/048.gif",
+    "pelle" : "http://www.jkry.org/~drsnu/smoke/pellesmoke.gif",
+    "vaikeapaatos" : "http://www.jkry.org/~drsnu/think/vaikeapaatos3.gif",
+    "wank" : "http://www.jkry.org/~drsnu/liebe/2s0cn75.jpg.gif",
+    "hyvahyvahyva" : "http://www.jkry.org/~drsnu/neutral/appn.gif",
+    "marssinatsi" : "http://www.jkry.org/~drsnu/neutral/nazimarssi2.gif",
+    "cooleek" : "http://www.jkry.org/~drsnu/cool/cooleek2rf.gif",
+    "vesa" : "http://www.jkry.org/~drsnu/nerd/postaajayk8.gif",
+    "clint" : "http://www.jkry.org/~drsnu/smoke/clintsmoke6sr.gif",
+    "lock" : "http://images.starcraftmazter.net/4chan/for_forums/in_before_lock.gif",
+    "dew" : "http://kabul.pp.fi/kuvia/hassuja/dewpojanhuikka.gif",
+    "viivalle" : "http://www.saunalahti.fi/~kohina/kuveja/viivalle.gif",
+    "kieli" : "http://threadbombing.com/data/media/2/big_tongue.gif",
+    "batman" : "http://www.saunalahti.fi/~kohina/kuveja/batmanpeukku.gif",
+    "commando" : "http://www.eroticsupport.net/temp/commando.gif",
+    "torille" : "http://i41.tinypic.com/e8ljl3.gif",
+    "arnold" : "http://i224.photobucket.com/albums/dd284/mara_p/6sbphmt-1.gif",
+    "nyyppapostaa" : "http://www.saunalahti.fi/~kohina/kuveja/nyyppapostaa.gif",
+    "gtfo" : "http://i203.photobucket.com/albums/aa168/ThisIsNotDan91/GTFO.gif",
+    "lolpuke" : "http://img339.imageshack.us/img339/7011/hahalolpukewa2.gif",
+    "juti" : "http://i.imgur.com/vezSW.jpg",
+    "nakki" : "http://www.saunalahti.fi/~kohina/kuveja/nakki.gif",
+    "horp" : "http://i47.tinypic.com/2hppy6u.gif",
+    "patesmoke" : "http://i313.photobucket.com/albums/ll380/Rapist-search/patesmoke.gif",
+    "ghehe" : "http://www.netikka.net/jorge/hymiot/misc/ghehehe.gif",
+    "kahvi" : "http://www.netikka.net/jorge/hymiot/misc/riukuessenfk8.gif",
+    "nappi" : "http://www.netikka.net/jorge/hymiot/misc/nappi.gif",
+    "mkone" : "http://www.netikka.net/jorge/hymiot/kone/hehe.gif",
+    "ckiss" : "http://www.netikka.net/jorge/hymiot/cool/coolkiss9mk.gif",
+    "nguit" : "http://www.netikka.net/jorge/hymiot/neutral/guitarheronerd.gif",
+    "tasku" : "http://www.netikka.net/jorge/hymiot/neutral/neutauki6gb.gif",
+    "broke" : "http://www.netikka.net/jorge/hymiot/neutral/neutauki6gb.gif",
+    "isonene" : "http://www.netikka.net/jorge/hymiot/neutral/bnene.gif",
+    "juice" : "http://www.netikka.net/jorge/hymiot/neutral/juice.gif",
+    "assa" : "http://www.netikka.net/jorge/hymiot/neutral/assa.gif",
+    "coolkyl" : "http://www.netikka.net/jorge/hymiot/neutral/yesyescool.gif",
+    "rneut" : "http://www.netikka.net/jorge/hymiot/neutral/rneut.gif",
+    "droolliebe" : "http://www.netikka.net/jorge/hymiot/liebe/droolliebe.gif",
+    "raps" : "http://www.netikka.net/jorge/hymiot/think/lolwtf.gif",
+    "nthink" : "http://www.netikka.net/jorge/hymiot/think/thinkn.gif",
+    "minismoke" : "http://www.netikka.net/jorge/hymiot/smoke/smoker.gif",
+    "coolsmoke" : "http://www.netikka.net/jorge/hymiot/smoke/coolsmoker7ny.gif",
+    "xsmoke" : "http://www.netikka.net/jorge/hymiot/smoke/coolsmoke73.gif",
+    "sasd" : "http://www.netikka.net/jorge/hymiot/smoke/coolsmoke73.gif",
+    "greensmoke" : "http://www.netikka.net/jorge/hymiot/smoke/smokemrgreen6os.gif",
+    "sir" : "http://www.netikka.net/jorge/hymiot/smoke/2217481.gif",
+    "massy" : "http://www.netikka.net/jorge/hymiot/gifs/33f.gif",
+    "letterman" : "http://www.netikka.net/jorge/hymiot/gifs/letter.gif",
+    "khyyl" : "http://www.netikka.net/jorge/hymiot/gifs/khyyl.gif",
+    "njaa" : "http://www.netikka.net/jorge/hymiot/gifs/njaa.gif",
+    "ooh" : "http://www.netikka.net/jorge/hymiot/gifs/midgetface.gif",
+    "mha" : "http://www.netikka.net/jorge/hymiot/gifs/wut.gif",
+    "wut" : "http://www.netikka.net/jorge/hymiot/gifs/wut.gif",
+    "jere" : "http://www.netikka.net/jorge/hymiot/gifs/jere.gif",
+    "vakio" : "http://www.netikka.net/jorge/hymiot/gifs/viivalle.gif",
+    "purkka" : "http://www.netikka.net/jorge/hymiot/gifs/purkka_a.gif",
+    "whoa" : "http://www.netikka.net/jorge/hymiot/gifs/italianspiderwhoa.gif",
+    "yhy" : "http://www.netikka.net/jorge/hymiot/gifs/itke_minulle_joki.gif",
+    "jaffa" : "http://www.netikka.net/jorge/hymiot/gifs/dewpojanhuikka.gif",
+    "jokerlach" : "http://www.netikka.net/jorge/hymiot/gifs/th_jokerlaugh.gif",
+    "en" : "http://www.netikka.net/jorge/hymiot/gifs/en.gif",
+    "jeffe" : "http://www.netikka.net/jorge/hymiot/gifs/jeffe.gif",
+    "simo" : "http://www.netikka.net/jorge/hymiot/gifs/cmo2-1.gif",
+    "naps" : "http://www.netikka.net/jorge/hymiot/gifs/avatar88299vi1.gif",
+    "tarkk" : "http://www.netikka.net/jorge/hymiot/gifs/timothyofthenight.gif",
+    "vneut" : "http://www.netikka.net/jorge/hymiot/neutral/neut6.gif",
+    "bada" : "http://img128.imageshack.us/img128/8312/emotrimshotgi9.gif",
+    "oujee" : "http://www.netikka.net/jorge/hymiot/misc/rhmm.gif",
+    "inv" : "http://tirpax.com/kema/kuvat/invertxd.gif",
+    "nuuska" : "http://www.netikka.net/jorge/hymiot/misc/nuuska2ct3al.gif",
+    "bbb" : "http://www.netikka.net/jorge/hymiot/misc/punttihn7.gif",
+    "culli" : "http://www.netikka.net/jorge/hymiot/cool/coolrg9.gif",
+    "hys" : "http://i.somethingawful.com/forumsystem/emoticons/emot-ssh.gif",
+    "colbert" : "http://i.somethingawful.com/forumsystem/emoticons/emot-colbert.gif",
+    "ulos" : "http://i42.tinypic.com/1z55361.gif",
+    "sikari" : "http://i41.tinypic.com/34sidl4.gif",
+    "brad" : "http://i.imgur.com/0Xz6o.gif",
+    "hate" : "http://i.imgur.com/zZogr.jpg",
+    "rkelly" : "http://www.pilipali.info/misc/rkellywtfio8.gif",
+    "sexophone" : "http://i.imgur.com/NE9rL.gif",
+    "frendit" : "http://kabul.pp.fi/kuvia/hassuja/cpsniper.gif",
+    "seinfeld" : "http://i329.photobucket.com/albums/l366/KZZHR/Giffit/seinfuckit.gif",
+    "patman" : "http://koti.kapsi.fi/murmeli47/patman.gif",
+    "hdtonni" : "http://i.imgur.com/bKkn0.jpg",
+    "trek" : "http://media.riemurasia.net/albumit/m00/700220939.gif",
+    "dumbpuke" : "http://www.netikka.net/jorge/hymiot/gifs/oksu.gif",
+    "nenehug" : "http://i.imgur.com/Kt9CA.gif",
+    "taryneut" : "http://www.netikka.net/jorge/hymiot/neutral/neut6.gif",
+    "barney" : "http://i.imgur.com/mirQS.gif",
+    "lolol" : "http://i179.photobucket.com/albums/w302/mcmilli4/lamontlaughing.gif",
+    "jaatelo" : "http://lh6.ggpht.com/_EXhBb0V30LA/TFndlO_Y-_I/AAAAAAAAB1w/2F-2hMQW5Uw/novodvor%20eats%20icecream.gif",
+    "mitav" : "http://img62.imageshack.us/img62/9142/thefuckw.gif",
+    "paivitatietosi" : "http://static.iltalehti.fi/uutiset/halonenvenajakansi1312HL_503_uu.jpg",
+    "halonen" : "http://static.iltalehti.fi/uutiset/halonenvenajakansi1312HL_503_uu.jpg",
+    "patepeukku" : "http://www.netikka.net/jorge/hymiot/tsoribraans/peukku.gif",
+    "otsa" : "http://i.imgur.com/fRGic.gif",
+    "becool" : "http://i.imgur.com/CsGDd.png",
+    "ehei" : "http://i42.tinypic.com/2rfvy1d.gif",
+    "grin" : "http://i.imgur.com/bfHzY.jpg",
+    "cry" : "http://i.imgur.com/Wlfag.gif",
+    "idilol" : "http://mwittmann.wippiespace.com/idi_reps.gif",
+    "jeps" : "http://mlkshk.com/r/PB5",
+    "billy" : "http://i.imgur.com/Kco8B.gif",
+    "iiam" : "http://i.imgur.com/uMeoD.gif",
+    "obama" : "http://i.imgur.com/zpGVt.gif",
+    "poni" : "http://i.imgur.com/KGEIX.gif",
+    "pere" : "http://i.imgur.com/to0Yx.gif",
+    "niiloneut" : "http://i.imgur.com/oUUYT.gif",
+    "coolje" : "http://i.imgur.com/kI9fG.gif",
+    "niilojoo" : "http://i.imgur.com/yNLVg.gif",
+    "jumalauta" : "http://i.imgur.com/VdRI8.jpg",
+    "paska" : "http://www.netikka.net/jorge/hymiot/psycholol/paskadancinggif5yp.gif",
+    "inno" : "http://i.imgur.com/jIf3h.jpg",
+    "drinkspede" : "http://www.freakdancerecords.net/spede/spededrink.gif",
+    "pilli" : "http://www.cosgan.de/images/smilie/sportlich/p033.gif",
+    "jalkatapsu" : "http://i.imgur.com/ZD9ZRqY.gif",
+    "starbucks" : "http://is11.snstatic.fi/img/658/1288620295413.jpg",
+    "why" : "http://i3.kym-cdn.com/entries/icons/original/000/002/085/Kornheiser_Why.JPG",
+    "pleb" : "http://i.imgur.com/dLWt57F.png",
+    "nautin" : "http://i.imgur.com/i3sKS0b.png",
+    "lapsi" : "http://i.imgur.com/5wAoHyz.jpg",
+    "karhunuuh" : "http://i2.photobucket.com/albums/y37/huuhoo/murr.jpg",
+    "mtvo" : "http://i.imgur.com/YnsHgwe.png",
+    "itvo" : "http://i.imgur.com/UlNUsij.jpg",
+    "diplomi" : "http://i.imgur.com/ZjiKxZ2.jpg",
+    "isoneut" : "http://i.imgur.com/w9a2x.gif",
+    "walk" : "http://i.imgur.com/g0q7MQg.gif",
+    "brotswana" : "http://i.imgur.com/DGcDenL.gif",
+    "tenho": "http://i.imgur.com/XdMmnkc.gif",
+    "sillu": "http://i.imgur.com/gXvDall.gif",
+    "patesiem": "http://i.imgur.com/Db6aqGA.gif",
+    "akijes": "http://i.imgur.com/6559eH6.gif",
+    "arje": "http://i.imgur.com/fm5WGLG.gif",
+    "oksu": "http://i.imgur.com/uNCMcnl.gif",
+    "vahvaa": "http://i.imgur.com/i0mzG7j.gif",
+    "huelldayum": "http://i.imgur.com/oMuMtZo.gif",
+    "markolol": "http://i.imgur.com/EmRPT.gif",
+    "pylly2": "https://media.giphy.com/media/lgkIEmOUL5PVu/giphy.gif",
+    "rage": "http://i.imgur.com/Y7PDvbm.gif",
+    "haundi": "http://i.imgur.com/nJMwn9q.gif",
+    "sammakko": "http://i.imgur.com/XRTQE.gif",
+    "lapsismoke": "http://i.imgur.com/sz1jHUN.gif",
+    "lapsismoke2": "http://i.imgur.com/kwRCZ.gif",
+    "piippu": "http://asdin.kapsi.fi/gifwall/gifs/lakupiippu2.gif",
+    "maverick": "http://i.imgur.com/ePcL3K6.gif",
+    "predatorlach": "http://i.imgur.com/GR6vi90.gif",
+    "damn": "http://i.imgur.com/4hzz9PY.gif",
+    "doakes": "http://i.imgur.com/n6luEld.gif",
+    "dwarfrage": "http://i.imgur.com/2N3OBbW.gif",
+    "heston": "http://i.imgur.com/OBrvFsU.gif",
+    "hhh": "http://i.imgur.com/xyUpgxh.gif",
+    "conan": "http://i.imgur.com/QUht8Uc.gif",
+    "cage": "http://i.imgur.com/FrADkjj.gif",
+    "coleman": "http://i.imgur.com/aTG7Z03.gif",
+    "ketonen": "http://i.imgur.com/19IH0I2.gif",
+    "dogslap": "http://i.imgur.com/tFZHETc.gif",
+    "dayum": "http://i.imgur.com/QrscZ.gif",
+    "pylly": "http://i.imgur.com/gXuUCIf.gif",
+    "siem": "http://i.imgur.com/ND2tVaq.gif",
+    "landa": "http://i.imgur.com/D4yzjTH.gif",
+    "seinfeld": "http://i.imgur.com/ucuRP6R.gif",
+    "lepo": "http://i.imgur.com/GOEpzkq.jpg"
+ };
+
+ function split(val) {
+     return val.split(/\s*::/);
+ }
+
+ function extractLast(term) {
+     return split(term).pop();
+ }
+
+ $.ui.autocomplete.prototype._resizeMenu = function () {
+     var ul = this.menu.element;
+     ul.outerWidth(this.element.outerWidth());
+ };
+
+ $.ui.autocomplete.filter = function (array, term) {
+     var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex(term), "i");
+     return $.grep(array, function (value) {
+         return matcher.test(value.label || value.value || value);
+     });
+ }
+
+ $("textarea[name='message']").bind("keydown", function (event) {
+     if (event.keyCode === $.ui.keyCode.TAB &&
+         $(this).data("ui-autocomplete").menu.active) {
+         event.preventDefault();
+     }
+ }).autocomplete({
+     minLength: 3,
+     delay: 100,
+     search: function () {
+         // custom minLength
+         var term = extractLast(this.value);
+         //var terms = split(this.value);
+         if (term.length < 1 || term === this.value) {
+             return false;
+         }
+     },
+     source: function (request, response) {
+         // delegate back to autocomplete, but extract the last term
+         response($.ui.autocomplete.filter(
+             Object.keys(smileys), extractLast(request.term)));
+     },
+     focus: function () {
+         // prevent value inserted on focus
+         return false;
+     },
+     select: function (event, ui) {
+        var n = this.value.lastIndexOf("::");
+        if (n != -1) {
+            this.value = this.value.substring(0, n).concat("[IMG]", smileys[ui.item.value], "[/IMG] ");
+            return false;
+        }
+     }
+ }).data("ui-autocomplete")._renderItem = function (ul, item) {
+     ul.css("list-style-type", "none");
+     return $("<li>")
+         .css("background-color", "#363636")
+         .append($("<a>").text(item.label).append("&nbsp;<img style='max-height: 30px; max-width: 30px;' src='" + smileys[item.value] + "' />"))
+         .appendTo(ul);
+ };
+ $("textarea[name='message']").blur(function () {
+     for (smiley in smileys) {
+         var tag = new RegExp("::" + smiley, "gi");
+         var img = "[IMG]" + smileys[smiley] + "[/IMG]";
+         $(this).val(function (i, val) {
+             return val.replace(tag, img);
+         });
+     }
+ });
